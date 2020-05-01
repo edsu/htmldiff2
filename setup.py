c32bc3e2a8768906e3a596cc02eaf79147b740d9
Hugo <hugovk@users.noreply.github.com>
2020-05-01 21:35:00 +0300

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Ed Summers',
     author_email='ehs@pobox.com',
     version='0.1.1',
-    url='http://github.com/edsu/htmldiff2',
+    url='https://github.com/edsu/htmldiff2',
     py_modules=['htmldiff2'],
     description='Diffs arbitrary HTML inline.',
     long_description=long_description,
@@ -16,6 +16,7 @@ setup(
     zip_safe=False,
     install_requires=['genshi', "html5lib"],
     test_suite='test',
+    python_requires=">=3.5",
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python'
