6bc38ea7743359b0ff31f4f2bdfbc4704e5c84ce
Ed Summers <ehs@pobox.com>
2020-05-01 10:15:57 -0400

    include markdown for pypi

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
     name='htmldiff2',
     author='Ed Summers',
     author_email='ehs@pobox.com',
-    version='0.1',
+    version='0.1.1',
     url='http://github.com/edsu/htmldiff2',
     py_modules=['htmldiff2'],
     description='Diffs arbitrary HTML inline.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     zip_safe=False,
     install_requires=['genshi', "html5lib"],
     test_suite='test',
